a3e165388bff180aeadb3aa85a66581eece4ec91
armin <arminko82@gmail.com>
2017-10-22 20:25:25 +0200

    Intermedaite save, contains new code to make nodes with jsdom

--- test/main-spec.js
+++ test/main-spec.js
@@ -39,7 +39,7 @@ describe('MarkUpCreation', function() {
 describe('ShallowDomElementsCopy', function() {
     it('should create a shallow copy of all DOM nodes in given document\'s body', function() {
     		var input = ["<p>Hello world</p>", "<p>Hello my world</p>", "<p></p>"];
-        document.body.innerHTML = input.join(); // document from jsdom-global 
+        document.body.innerHTML = input.join(); // document from jsdom-global
         var expected = input;
         var actual = mmw.getShallowElementsCopy();
         var actualStrings = actual.map(x => x.outerHTML);
@@ -47,6 +47,39 @@ describe('ShallowDomElementsCopy', function() {
         expect(actualStrings).to.deep.equal(expected);
     });
 });
+
+describe('MarkUpSingleNodeText', function() {
+    it('should take a single html node and apply the highlight on it', function() {
+		var highlight = "world";
+		var inputs = [ 
+			"<p>Hello world</p>", 
+			"<p>Hello my world</p>", 
+			"<p></p><!-- is the child of this p null? -->"];
+		var outputs = [ 
+			"<p>Hello <mark>world</mark></p>", 
+			"<p>Hello my <mark>world</mark></p>", 
+			"<p></p><!-- is the child of this p null? -->"];
+
+		assert(inputs.length !== outputs.length, "Test data error");
+    
+        var p = document.createElement('p');
+        var text = document.createTextNode("Hello ");
+        var mark = document.createElement('mark');
+        var markText = document.createTextNode(highlight);
+        mark.appendChild(markText);
+        p.appendChild(text);
+        p.appendChild(mark);
+
+		var input = "<p>Hello " + highlight + "</p>";
+        document.body.innerHTML = input;
+        var expected = p.outerHTML;
+        mmw.changeNode(document.body.firstChild, highlight, ["Hello ", highlight]);
+        var actual = document.body.innerHTML;
+        
+        expect(expected).to.equal(actual);
+    });
+});
+
 
 
 describe('MarkWorldInTdd1', function () {
